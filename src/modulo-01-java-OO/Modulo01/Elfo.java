6e34f18e230477dc0afc4409349f5cd2b3bef0ec
EmmanoelF <emmanoel_95@hotmail.com>
2015-09-15 22:18:00 -0300

    Exercicios das flechas

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,9 +1,21 @@
 public class Elfo {
     private String nome;
     private int flechas;
+    private double experiencia;
     
     public Elfo(String n, int flech) {
         nome = n;
         flechas = flech;
     }
+    
+    public Elfo(String n){
+        nome = n;
+        flechas = 42;
+    }
+    
+    public void atirarFlechar(){
+        flechas = flechas -1;
+        experiencia = experiencia +1;
+    }
+    
 }
