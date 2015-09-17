93e8cf4f71bf40c205f1a450e146e14a13ac8745
EmmanoelF <emmanoel_95@hotmail.com>
2015-09-16 23:34:56 -0300

    feat[DwarfTest] verificaSeTomouDano

--- src/modulo-01-java-OO/Modulo01/DwarfTest.java
+++ src/modulo-01-java-OO/Modulo01/DwarfTest.java
@@ -20,12 +20,19 @@ public class DwarfTest{
     }
     
     @Test
-    public void testaDanoFlechada(){
+    public void reduz10DeVidaAposFlechada(){
         Dwarf d = new Dwarf();
         int aux1 = d.getVida();
         d.receberFlechada();
         int aux2 = d.getVida();
         int aux3 = aux2-aux1;
         assertEquals(-10, aux3);
+    }
+    
+    @Test
+    public void verificaSeDwarfTomouDano(){
+        Dwarf d = new Dwarf();
+        d.receberFlechada();
+        assertEquals(100, d.getVida());
     }
 }
