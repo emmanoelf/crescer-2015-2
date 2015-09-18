182a3e029b96fcca2b546d49b2cc687c479fe0f0
EmmanoelF <emmanoel_95@hotmail.com>
2015-09-17 23:39:42 -0300

    style[Dwarf]

--- src/modulo-01-java-OO/Modulo01/Dwarf.java
+++ src/modulo-01-java-OO/Modulo01/Dwarf.java
@@ -4,44 +4,44 @@ public class Dwarf{
     private String nome;
     private int experiencia;
     private DataTerceiraEra dataNascimento;
-    
+
     public Dwarf(String nome){
         this();
         this.nome = nome;
     }
-    
+
     public Dwarf(String nome, DataTerceiraEra dataNascimento){
         this();
         this.nome = nome;
         this.dataNascimento = dataNascimento;
     }
-    
+
     public Dwarf(){
         this.status = status.VIVO;
         this.vida = 110;
         this.dataNascimento = new DataTerceiraEra(1,1,1);
     }
-    
+
     public int getVida(){
         return this.vida;
     }
-    
+
     public Status getStatus(){
         return this.status;
     }
-    
+
     public String getNome(){
         return this.nome;
     }
-    
+
     public int getExperiencia(){
         return this.experiencia;
     }
-    
+
     public DataTerceiraEra getDataNascimento(){
         return this.dataNascimento;
     }
-    
+
     public void receberFlechada(){
         double sorteLancada = getNumeroSorte();
         if(sorteLancada < 0){
@@ -58,15 +58,15 @@ public class Dwarf{
             }
         }
     }
-    
-   public double getNumeroSorte(){
-       double valorInicial = 101.0;
-       if(this.getDataNascimento().ehBissexto() && (this.vida >=80 && this.vida <=90)){
-           return (valorInicial) * (-33);
+
+    public double getNumeroSorte(){
+        double valorInicial = 101.0;
+        if(this.getDataNascimento().ehBissexto() && (this.vida >=80 && this.vida <=90)){
+            return (valorInicial) * (-33);
         }
-       if(!this.getDataNascimento().ehBissexto() && (this.nome.equals("Seixas") || this.nome.equals("Meireles"))){
-           return (valorInicial * 33) %100;
+        if(!this.getDataNascimento().ehBissexto() && (this.nome.equals("Seixas") || this.nome.equals("Meireles"))){
+            return (valorInicial * 33) %100;
         }
-       return valorInicial;
+        return valorInicial;
     }
 }
