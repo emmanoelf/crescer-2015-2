b7ee1046c874cae1dd91c438925fda4ac9c6c98d
EmmanoelF <emmanoel_95@hotmail.com>
2015-09-17 23:14:53 -0300

    feat[DataTerceiraEraTest] novo teste unitario

---
+++ src/modulo-01-java-OO/Modulo01/DataTerceiraEraTest.java
@@ -0,0 +1,37 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class DataTerceiraEraTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class DataTerceiraEraTest{
+    
+    @Test
+    public void dia15DeAbrilDe2003(){
+        DataTerceiraEra dte = new DataTerceiraEra(15,4,2003);
+        assertEquals(15, dte.getDia());
+        assertEquals(4, dte.getMes());
+        assertEquals(2003, dte.getAno());
+    }
+    
+    @Test
+    public void ano2008EhBissexto(){
+        boolean bissexto;
+        bissexto = new DataTerceiraEra(5,9,2008).ehBissexto();
+        assertTrue(bissexto);
+    }
+    
+    @Test
+    public void ano2011NaoEhBissexto(){
+        boolean bissexto;
+        bissexto = new DataTerceiraEra(19,11,2011).ehBissexto();
+        assertFalse(bissexto);
+    }
+}
