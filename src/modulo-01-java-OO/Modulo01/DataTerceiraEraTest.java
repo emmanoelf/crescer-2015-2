0489555732fbe35b2810082691878b577fb2f811
EmmanoelF <emmanoel_95@hotmail.com>
2015-09-18 01:44:59 -0300

    docs[DataTerceiraEraTest] removido comentarios padroes do bluej

--- src/modulo-01-java-OO/Modulo01/DataTerceiraEraTest.java
+++ src/modulo-01-java-OO/Modulo01/DataTerceiraEraTest.java
@@ -1,16 +1,9 @@
 
-
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * The test class DataTerceiraEraTest.
- *
- * @author  (your name)
- * @version (a version number or a date)
- */
 public class DataTerceiraEraTest{
     
     @Test
