c64c01ad16899000058366ffbf3f08e9a2b4ec4c
EmmanoelF <emmanoel_95@hotmail.com>
2015-09-18 22:36:17 -0300

    test[ItemTest] adicionando testes com quantidade negativa e descricao null

--- src/modulo-01-java-OO/Modulo01/Item.java
+++ src/modulo-01-java-OO/Modulo01/Item.java
@@ -2,9 +2,9 @@ public class Item{
     private String descricao;
     private int quantidade;
     
-    public Item(String d, int q){
-        this.descricao = d;
-        this.quantidade = q;
+    public Item(String descricao, int quantidade){
+        this.descricao = descricao;
+        this.quantidade = quantidade;
     }
     
     public String getDescricao(){
--- src/modulo-01-java-OO/Modulo01/ItemTest.java
+++ src/modulo-01-java-OO/Modulo01/ItemTest.java
@@ -11,4 +11,17 @@ public class ItemTest{
         assertEquals("Adaga", i.getDescricao());
         assertEquals(2, i.getQuantidade());
     }
+    
+    @Test
+    public void itemCriadoComQuantidadeNegativa(){
+        Item i = new Item("Potion", -5);
+        int quantidadeEsperada = -5;
+        assertEquals(quantidadeEsperada, i.getQuantidade());
+    }
+    
+    @Test
+    public void itemCriadoComDescricaoNull(){
+        Item i = new Item(null, 9);
+        assertEquals(null, i.getDescricao());
+    }
 }
