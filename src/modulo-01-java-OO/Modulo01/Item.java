89d1d5f8addc4f7420c0ad4e2883188dad8c1c4a
Emmanoel F <emmanoel_95@hotmail.com>
2015-09-21 17:40:15 -0300

    feat[Item] ganha1000UnidadesESoma

--- src/modulo-01-java-OO/Modulo01/Item.java
+++ src/modulo-01-java-OO/Modulo01/Item.java
@@ -1,21 +1,37 @@
 public class Item{
     private String descricao;
     private int quantidade;
-    
+
     public Item(String descricao, int quantidade){
         this.descricao = descricao;
         this.quantidade = quantidade;
     }
-    
+
     public String getDescricao(){
         return this.descricao;
     }
-    
+
     public int getQuantidade(){
         return this.quantidade;
     }
-    
+
     public void ganha1000Unidades(){
         this.quantidade += 1000;
     }
+
+    public void ganha1000UnidadesESoma(){
+        int contador = 0;
+        for(int i = 0; i <= this.quantidade; i++){
+            contador += i;
+        }
+        this.quantidade += contador;
+        contador *= 1000;
+        this.quantidade += contador;
+    }
+   /*
+    public void shimbalaie() {
+        int pa = this.quantidade * (this.quantidade+1) / 2;
+        this.quantidade += 1000 * pa + pa;
+    }
+    */
 }
