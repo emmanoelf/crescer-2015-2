ba24fc0cb710f6884ab718194f5ed4e88d71d0cc
Emmanoel F <emmanoel_95@hotmail.com>
2015-09-22 17:27:43 -0300

    feat[Personagem]

---
+++ src/modulo-01-java-OO/Modulo01/Personagem.java
@@ -0,0 +1,44 @@
+public class Personagem{
+    protected String nome;
+    protected int vida;
+    protected int experiencia;
+    protected Status status;
+    protected Inventario inventario;
+
+    public Personagem(){
+        this.inventario = new Inventario();
+    }
+
+    public Inventario getInventario(){
+        return this.inventario;
+    }
+    
+    public void adicionarItem(Item item){
+        this.inventario.adicionarItem(item);
+    }
+    
+    public void removerItem(Item item){
+        this.inventario.perderItem(item);
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public int getVida(){
+        return this.vida;
+    }
+    
+    public int getExperiencia(){
+        return this.experiencia;
+    }
+    
+    public Status getStatus(){
+        return this.status;
+    }
+    
+        public void receberAtaqueDoOrc(Orc orc){
+        int dano = orc.getDanoDeAtaque();
+        this.vida -= dano;
+    }
+}
