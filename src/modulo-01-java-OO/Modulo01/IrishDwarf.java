22ecdce5c3f48d454b373d226a18a16a6a814c1d
EmmanoelF <emmanoel_95@hotmail.com>
2015-09-22 12:24:11 -0300

    ajustado heranca

--- src/modulo-01-java-OO/Modulo01/IrishDwarf.java
+++ src/modulo-01-java-OO/Modulo01/IrishDwarf.java
@@ -1,22 +1,17 @@
 public class IrishDwarf extends Dwarf{
-
-    
-    public IrishDwarf(){
-        
-    }
     
     public IrishDwarf(String nome){
-        
+        super(nome);
     }
 
     public IrishDwarf(String nome, DataTerceiraEra dataNascimento){
-        
+        super(nome, dataNascimento);
     }
 
     public void tentarSorte(){
         if(this.getNumeroSorte() == -3333.0){
             for(int i = 0; i < inventario.listaDeItens.size(); i++){
-                this.inventario.getItens().get(i).shimbalaie();
+                this.inventario.getItens().get(i).ganha1000UnidadesESoma();
             }
         }
     }
