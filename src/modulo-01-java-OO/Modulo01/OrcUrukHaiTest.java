dca19334ffeb2465bec6c8022ad84e0a341315bd
EmmanoelF <emmanoel_95@hotmail.com>
2015-09-24 20:50:15 -0300

    Test(OrcUrukHaiTest) adicionado alguns testes

--- src/modulo-01-java-OO/Modulo01/OrcUrukHaiTest.java
+++ src/modulo-01-java-OO/Modulo01/OrcUrukHaiTest.java
@@ -3,39 +3,32 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class OrcUrukHaiTest{
     @Test
     public void criaUrukHai(){
         OrcUrukHai orcUrukHai = new OrcUrukHai("Ukarggoth");
-        
         int vidaEsperada = 150;
         Inventario inventarioEsperado = new Inventario();
         inventarioEsperado.adicionarItem(new Item("Escudo Uruk-Hai",1));
         inventarioEsperado.adicionarItem(new Item("Espada",1));
-        
         assertEquals(vidaEsperada, orcUrukHai.getVida(), 0.1);
         assertEquals(inventarioEsperado, orcUrukHai.getInventario());
     }
-    
+
     @Test
     public void orcUrukHaiRecebeDanoDeAnao(){
         OrcUrukHai orcUrukHai = new OrcUrukHai("Ukarggoth");
-        
         orcUrukHai.receberDano();
-        
         assertEquals(144, orcUrukHai.getVida(), 0.1);
     }
-    
+
     @Test
     public void orcUrukHaiRecebeDanoDeElfo(){
         OrcUrukHai orcUrukHai = new OrcUrukHai("Ukarggoth");
-        
         orcUrukHai.receberDano();
-        
         assertEquals(144, orcUrukHai.getVida(), 0.1);
     }
-    
+
     @Test
     public void orcUrukHaiMorre(){
         OrcUrukHai orcUH = new OrcUrukHai("Thosburogg");
@@ -45,17 +38,17 @@ public class OrcUrukHaiTest{
         }
         assertEquals(vidaEsperada, orcUH.getVida(), 0.1);
         assertEquals(Status.MORTO, orcUH.getStatus());
-   }
-   
-   @Test
-   public void orcUrukHaiAtacaElfo(){
-       OrcUrukHai orcUH = new OrcUrukHai("Thosburogg");
-       Elfo f1 = new Elfo("Legolas");
-       int vidaEsperada = 88;
-       orcUH.atacarPersonagem(f1);
-       assertEquals(vidaEsperada, f1.getVida(), 0.1);
     }
-    
+
+    @Test
+    public void orcUrukHaiAtacaElfo(){
+        OrcUrukHai orcUH = new OrcUrukHai("Thosburogg");
+        Elfo f1 = new Elfo("Legolas");
+        int vidaEsperada = 88;
+        orcUH.atacarPersonagem(f1);
+        assertEquals(vidaEsperada, f1.getVida(), 0.1);
+    }
+
     @Test
     public void orcUrukHaiAtacaDwarf(){
         OrcUrukHai orcUH = new OrcUrukHai("Thosburogg");
@@ -63,5 +56,47 @@ public class OrcUrukHaiTest{
         int vidaEsperada = 98;
         orcUH.atacarPersonagem(d1);
         assertEquals(vidaEsperada, d1.getVida(), 0.1);
+    }
+    
+    @Test
+    public void orcUrukHaiPerdeEspadaEAtacaComArcoQueGanhouEUmaFlecha(){
+        Orc orc = new OrcUrukHai("Thosburogg");
+        Dwarf anao = new Dwarf("Falstad");
+        orc.removerItem(new Item("Espada", 1));
+        orc.adicionarItem(new Item("Arco", 1));
+        orc.adicionarItem(new Item("Flecha", 1));
+        Inventario inventarioEsperado = new Inventario();
+        inventarioEsperado.adicionarItem(new Item("Escudo Uruk-Hai", 1));
+        inventarioEsperado.adicionarItem(new Item("Arco", 1));
+        orc.atacarPersonagem(anao);
+        assertEquals(102, anao.getVida(), 0.0);
+        assertEquals(inventarioEsperado, orc.getInventario());
+    }
+    
+    @Test
+    public void orcUrukHaiFogePorqueNaoTemArmaParaAtacar(){
+        Orc orc = new OrcUrukHai("Thosburogg");
+        orc.removerItem(new Item("Espada", 1));
+        Elfo legolas = new Elfo("Legolas");
+        double vidaEsperada = 100;
+        orc.atacarPersonagem(legolas);
+        assertEquals(Status.FUGINDO, orc.getStatus());
+        assertEquals(vidaEsperada, legolas.getVida(), 0.0);
+    }
+    
+    @Test
+    public void orcUrukHaiRecebeAtaqueDeUrukHai(){
+        Orc urukHaiAlvo = new OrcUrukHai("Thosburogg");
+        Orc urukHaiAtacante = new OrcUrukHai("Muradin");
+        urukHaiAlvo.receberAtaqueDoOrc(urukHaiAtacante);
+        assertEquals(urukHaiAlvo.getVida(), 144, 0.0);
+    }
+    
+    @Test
+    public void orcUrukHaiRecebeAtaqueDeSnaga(){
+        Orc urukHaiAlvo = new OrcUrukHai("Thosburogg");
+        Orc snagaAtacante = new OrcSnaga("Falstad");
+        urukHaiAlvo.receberAtaqueDoOrc(snagaAtacante);
+        assertEquals(urukHaiAlvo.getVida(), 144, 0.0);
     }
 }
