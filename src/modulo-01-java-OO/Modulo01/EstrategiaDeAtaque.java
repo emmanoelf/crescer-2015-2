08fcbb030064f71bbb1c91298f2b8cdb3ad76ac5
Emmanoel F <emmanoel_95@hotmail.com>
2015-09-25 17:39:32 -0300

    feat(EstrategiaDeAtaque)

---
+++ src/modulo-01-java-OO/Modulo01/EstrategiaDeAtaque.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public interface EstrategiaDeAtaque{
+    
+    ArrayList<Elfo> getOrdemDoUltimoAtaque();
+    void atacar(ArrayList<Elfo> elfos, ArrayList<Dwarf> dwarves);
+    
+}
