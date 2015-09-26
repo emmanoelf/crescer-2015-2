6fb2c06313245b0c0394f1ea8977142a649a2f07
EmmanoelF <emmanoel_95@hotmail.com>
2015-09-26 20:04:58 -0300

    refactor(EstrategiaNormal) parametros do atacar alterado para exercitoDeElfos pois fica melhor de efetuar a busca por Status.VIVO

--- src/modulo-01-java-OO/Modulo01/EstrategiaNormal.java
+++ src/modulo-01-java-OO/Modulo01/EstrategiaNormal.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
 public class EstrategiaNormal implements EstrategiaDeAtaque{
     private ArrayList<Elfo> ordemDoUltimoAtaque = new ArrayList<>();
-    
+
     public ArrayList<Elfo>getOrdemDoUltimoAtaque(){
         return ordemDoUltimoAtaque;
     }
-    
-    public void atacar(ArrayList<Elfo> elfos, ArrayList<Dwarf> dwarves){
-        for(Elfo elfo : elfos){
+
+    public void atacar(ExercitoDeElfos exercito, ArrayList<Dwarf> dwarves){
+        ordemDoUltimoAtaque.clear();
+        exercito.agruparPorStatus();
+        ArrayList<Elfo> elfosVivos = exercito.buscar(Status.VIVO);
+        for(Elfo elfo : elfosVivos){
             ordemDoUltimoAtaque.add(elfo);
-                for(Dwarf dwarf : dwarves){
-                    elfo.atirarFlecha(dwarf);
-                }
+            for(Dwarf dwarf : dwarves){
+                elfo.atirarFlecha(dwarf);
             }
+        }
     }
 }
