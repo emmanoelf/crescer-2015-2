36be228e288787fee45bb1ef654275bc9ea4a7e2
EmmanoelF <emmanoel_95@hotmail.com>
2015-10-21 19:48:29 -0200

    feat(CarrinhoDeCompras) functions adicionarItem, removerItem, atualizarQuantidade, sortearDesconto e calcularValorTotal

---
+++ src/modulo-03-web/dia-03/CarrinhoDeCompras.js
@@ -0,0 +1,37 @@
+function CarrinhoDeCompras(){
+  this.itens = [];
+};
+
+CarrinhoDeCompras.prototype.adicionarItem = function(item){
+    this.itens.push(item);
+  };
+
+CarrinhoDeCompras.prototype.removerItem = function(skuInformada){
+  for(var i = 0, len = this.itens.length; i < len; i++){
+    if(this.itens[i].sku === skuInformada){
+      this.itens.splice(i, 1);
+    }
+  }
+};
+
+CarrinhoDeCompras.prototype.atualizarQuantidade = function (skuInformada, novaQuantidade){
+  return this.itens.map(function(item){
+    if(item.sku === skuInformada){
+      return item.quantidade = novaQuantidade;
+    }
+  });
+};
+
+CarrinhoDeCompras.prototype.sortearDesconto = function(){
+  return Math.random() < 0.4;
+}
+
+CarrinhoDeCompras.prototype.calcularValorTotal = function(){
+  var total = this.itens.map(function(item){
+    return item.calcularSubTotal();
+  })
+  .reduce(function(acumulador, item){
+    return acumulador + item;
+  });
+  return this.sortearDesconto()? (total *= 0.9) : total;
+};
