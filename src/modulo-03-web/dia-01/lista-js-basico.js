0774d1e3ccf36a987e2ca5f73f206d832eb0371b
EmmanoelF <emmanoel_95@hotmail.com>
2015-10-21 11:50:33 -0200

    refactor(lista-js-basico)alteradas as funções daisyGame, maiorTextos, imprime, fibonacci, fibosum, excelis, queroCafe

--- src/modulo-03-web/dia-01/lista-js-basico.js
+++ src/modulo-03-web/dia-01/lista-js-basico.js
@@ -1,32 +1,99 @@
-function daisyGame(numeroDePetalas){
-   if(numeroDePetalas %2 === 0){
-     return "Love me";
-   }else{
-     return "Love me not";
-   }
-}
+function isNumber(number) {
+  return typeof number === 'number';
+  // return !isNaN(number);
+};
 
-function maiorTexto(array){
-  var maior = 0;
-  for (var prop in array) {
-    if(typeof array[prop] === 'string'){
-      if (array[maior].length < array[prop].length){
-        maior = prop;
-      }
-    }
+function isFunction(fn) {
+  return typeof fn === 'function';
+};
+
+function daisyGame(petals) { console.log('inline'); };
+
+var daisyGame = function(petals) {
+  if (isNumber(petals)) {
+    return 'Love me' + ( petals % 2 === 0 ? ' not' : '' );
+  } else {
+    throw new Error('Petals not a number!');
   }
-  return array[maior];
+};
+
+var arr = ['t1', 'maior t3xt0', 't2a', 'u', '123'];
+arr['nome'] = 'Array querido';
+
+for (var prop in arr) {
+  console.log(arr[prop]);
 }
 
+(function maiorTexto(textos) {
 
-function imprime(array, func){
-	if(typeof func === 'function'){
-		for(var prop in array){
-			if(typeof array[prop] === 'string'){
-				func(array[prop])
-			}
-		}
-	}else{
-		return 'Jabulani: TypeError: ' + typeof func + ' is not a function';
-	}
-}
+  for (var i = 0, maiorTxt = '', len = textos.length; i < len; i++) {
+    (function() {
+      var dentro = 'fora';
+      if (textos[i].length > maiorTxt.length) {
+        maiorTxt = textos[i];
+      }
+      //console.log(textos[i]);
+    })();
+  }
+
+  //console.log('fora: %o', dentro);
+
+  return maiorTxt;
+})(arr);
+
+function imprime(instrutores, fn) {
+  // debugger;
+  if (isFunction(fn)) {
+    instrutores.forEach(fn);
+  }
+};
+
+var imprimeInstrutor = function(instrutor) {
+  console.log(instrutor);
+};
+
+var alerta = function(instrutor) {
+  alert(instrutor);
+};
+
+imprime(['bernardo', 'nunes', 'benhur'], imprimeInstrutor);
+imprime(['bernardo', 'nunes', 'benhur'], 'oi');
+// ERRADO:
+// imprime(['bernardo', 'nunes', 'benhur'], imprimeInstrutor());
+
+var fibonacci = function(n) {
+  if (n === 1) return 1;
+  if (n === 2) return 1;
+
+  return fibonacci(n-1)+fibonacci(n-2);
+};
+
+// fiboSum(7) => 13+8+5+3+2+1+1
+
+var fiboSum = function(n) {
+  //if (n === 1) return 1;
+  //return fibonacci(n) + fiboSum(n-1);
+  return fibonacci(n+2)-1;
+};
+
+var excelis = function(referencia) {
+  referencia = referencia.toUpperCase();
+  for (var i=0, soma=0, len=referencia.length; i < len; i++) {
+    soma = soma * 26 + (referencia.charCodeAt(i) - 64);
+  }
+  return soma;
+};
+
+function queroCafe(mascada, precos) {
+  return precos
+    .filter(function(elem) {
+      return elem <= mascada;
+    })
+    .sort(function(elem1, elem2) {
+      return elem1 > elem2;
+    })
+    .join(',');
+    // alternativas:
+    // join()
+    // toString()
+};
