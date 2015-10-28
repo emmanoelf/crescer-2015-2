41d1d12e48a20ecb150ca2e3b72eb09fc9a5c5d0
emmanoelf <emmanoel_95@hotmail.com>
2015-10-28 12:12:39 -0200

    testes unitarios até a letra C

---
+++ src/modulo-04-c-sharp/dia-02/DbFuncionarios/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DbFuncionarios;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PrimeiroCargoEAnalista()
+        {
+            var bd = new BaseDeDados();
+            List<Funcionario> funcionarios = bd.Funcionarios;
+            var resultado = bd.BuscarOrdenadosPorCargo();
+            Assert.AreEqual("Analista", resultado[0].Cargo.Titulo);
+        }
+
+        [TestMethod]
+        public void BuscarFuncionarioPorNome()
+        {
+            var bd = new BaseDeDados();
+            List <Funcionario> funcionarios= bd.Funcionarios;
+            var resultado = bd.BuscarPorNome("Rafael");
+            Assert.AreEqual("Rafael Benetti", resultado[0].Nome);
+        }
+
+        [TestMethod]
+        public void BuscarFuncionarioPorParteDoNome()
+        {
+            var bd = new BaseDeDados();
+            List<Funcionario> funcionarios = bd.Funcionarios;
+            var resultado = bd.BuscarPorNome("nol");
+            Assert.AreEqual("Eduardo Arnold", resultado[0].Nome);
+        }
+
+    }
+}
