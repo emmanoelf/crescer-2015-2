0a97e7a7bec5aba4b4af4b0b3e1d4a17ed9fdc25
Emmanoel F. <emmanoel_95@hotmail.com>
2015-10-28 17:34:37 -0200

    Adicionando Locadora

---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Dominio/Genero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio
+{
+    public enum Genero
+    {
+        AVENTURA,
+        CORRIDA,
+        LUTA,
+        RPG
+    }
+}
---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.Dominio/Jogo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Dominio
+{
+    public class Jogo
+    {
+        public string Nome { get; set; }
+        public Genero Genero { get; set; }
+        public double Preco { get; private set; }
+
+        public Jogo(string nome, Genero genero, double preco)
+        {
+            this.Nome = nome;
+            this.Genero = genero;
+            this.Preco = preco;
+        }
+    }
+}
