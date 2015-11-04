3366ec4ca7b7220045edb96258e8651f32832633
Emmanoel F <emmanoel_95@hotmail.com>
2015-11-04 17:39:32 -0200

    Pull dos instrutores para edicao

---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Web.MVC/Models/RelatorioModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Web.MVC.Models
+{
+    public class RelatorioModel
+    {
+        public List<Jogos> ListaJogos { get; set; }
+        public int QuantidadeJogos { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+
+        public RelatorioModel()
+        {
+            var jogos = new List<Jogos>();
+        }
+    }
+}
