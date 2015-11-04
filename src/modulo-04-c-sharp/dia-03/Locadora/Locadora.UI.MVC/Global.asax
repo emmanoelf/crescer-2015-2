c27d9a38a45bc5a0e283a749a6499ae39f1a80af
emmanoelf <emmanoel_95@hotmail.com>
2015-11-04 11:51:52 -0200

    adicionado view de apresentacao, controllerHome, Relatoriomodel

---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Locadora.UI.MVC.MvcApplication" Language="C#" %>
---
+++ src/modulo-04-c-sharp/dia-03/Locadora/Locadora.UI.MVC/Models/RelatorioModel.cs
@@ -0,0 +1,26 @@
+﻿using Locadora.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.UI.MVC.Models
+{
+    public class RelatorioModel
+    {
+        public int QuantidadeDeJogos { get; set; }
+        public double PrecoMedio { get; set; }
+        public string JogoMaisCaro { get; set; }
+        public string JogoMaisBarato { get; set; }
+        public IList<Jogo> listaDeJogos { get; set; }
+
+        public RelatorioModel(IList<Jogo> listaJogos, int quantidadeDeJogos, double precoMedio, string jogoMaisCaro, string jogoMaisBarato)
+        {
+            listaDeJogos = listaJogos;
+            QuantidadeDeJogos = quantidadeDeJogos;
+            PrecoMedio = precoMedio;
+            JogoMaisCaro = jogoMaisCaro;
+            JogoMaisBarato = jogoMaisBarato;
+        }
+    }
+}
