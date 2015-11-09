e49258c88f8403e64cf6d8f57aec4135ded64429
Emmanoel F. <emmanoel_95@hotmail.com>
2015-11-09 18:04:39 -0200

    Adicionado Migrations

--- src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.ADO/JogoRepositorio.cs
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.ADO/JogoRepositorio.cs
@@ -35,7 +35,8 @@ namespace Locadora.Repositorio.ADO
                 comando.AddParam("paramNome", entidade.Nome);
                 comando.AddParam("paramPreco", entidade.Preco);
                 comando.AddParam("paramIdCategoria", (int)entidade.Categoria);
-                comando.AddParam("paramIdClienteLocacao", entidade.IdClienteLocacao);
+                //TODO ajustar cliente locacao
+                comando.AddParam("paramIdClienteLocacao", entidade.ClienteLocacao.Id);
                 comando.AddParam("paramDescricao", entidade.Descricao);
                 comando.AddParam("paramIdSelo", (int)entidade.Selo);
                 comando.AddParam("paramImagem", entidade.Imagem);
@@ -105,7 +106,8 @@ namespace Locadora.Repositorio.ADO
                 comando.AddParam("paramNome", entidade.Nome);
                 comando.AddParam("paramPreco", entidade.Preco);
                 comando.AddParam("paramIdCategoria", (int)entidade.Categoria);
-                comando.AddParam("paramIdClienteLocacao", entidade.IdClienteLocacao);
+                //TODO ajustar cliente locacao
+                comando.AddParam("paramIdClienteLocacao", entidade.ClienteLocacao.Id);
                 comando.AddParam("paramDescricao", entidade.Descricao);
                 comando.AddParam("paramIdSelo", (int) entidade.Selo);
                 comando.AddParam("paramImagem", entidade.Imagem);
@@ -144,8 +146,9 @@ namespace Locadora.Repositorio.ADO
         private Jogo ConverterDataReaderEmJogo(IDataReader reader)
         {
             var jogo = new Jogo(
-                id: Convert.ToInt32(reader["Id"]),
-                idClienteLocacao: reader["IdClienteLocacao"].ToString().ToNullable<int>()
+                id: Convert.ToInt32(reader["Id"])
+                //TODO ajustar cliente locacao
+                //ClienteLocacao: reader["ClienteLocacao"].ToString().ToNullable<int>()
                 );
 
             jogo.Nome = reader["Nome"].ToString();
---
+++ src/modulo-04-c-sharp/Locadora/Locadora.Repositorio.EF/BancoDeDados.cs
@@ -0,0 +1,57 @@
+﻿using Locadora.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Repositorio.EF
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Jogo> Jogo { get; set; }
+        public DbSet<Cliente> Cliente { get; set; }
+
+        public BancoDeDados() : base("LocadoraEF")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ClienteMap());
+            modelBuilder.Configurations.Add(new JogoMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    class JogoMap : EntityTypeConfiguration<Jogo>
+    {
+        public JogoMap()
+        {
+            ToTable("Jogo");
+            HasKey(jogo => jogo.Id);
+
+            Property(jogo => jogo.Nome).IsRequired().HasMaxLength(250);
+            Property(jogo => jogo.Preco).IsRequired();
+            Property(jogo => jogo.Descricao).IsRequired();
+            Property(jogo => jogo.Imagem).IsOptional();
+            Property(jogo => jogo.Video).IsOptional();
+            Property(jogo => jogo.Categoria).IsRequired().HasColumnName("IdCategoria");
+            Property(jogo => jogo.Selo).IsRequired().HasColumnName("IdSelo");
+        }
+    }
+
+    class ClienteMap : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteMap()
+        {
+            ToTable("Cliente");
+
+            HasKey(cliente => cliente.Id);
+            Property(cliente => cliente.Nome).IsRequired().HasMaxLength(250);
+        }
+    }
+}
