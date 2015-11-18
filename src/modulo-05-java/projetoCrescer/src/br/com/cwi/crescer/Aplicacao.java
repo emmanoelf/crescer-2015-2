336874859c33ede5c28f7092a2e05464766f175a
Emmanoel F <emmanoel_95@hotmail.com>
2015-11-18 12:17:58 -0200

    feat(Aplicacao) adicionado menu do primeiro exercicio

--- src/modulo-05-java/projetoCrescer/src/br/com/cwi/crescer/Aplicacao.java
+++ src/modulo-05-java/projetoCrescer/src/br/com/cwi/crescer/Aplicacao.java
@@ -1,8 +1,64 @@
 package br.com.cwi.crescer;
 
+import java.sql.SQLException;
+
+import br.com.cwi.crescer.dao.ClienteDao;
+import br.com.cwi.crescer.model.Cliente;
+
 public class Aplicacao {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
+        Cliente cliente = new Cliente();
+        cliente.setIdCliente(1L);
+        cliente.setNmCliente("Jão");
+        cliente.setNrCpf("231312321");
+        ClienteDao clienteDao = new ClienteDao();
+        clienteDao.insert(cliente);
+
+        /*
+         * ClienteDao clienteDao = new ClienteDao();
+         * List<Cliente> lista = clienteDao.listAll();
+         * for (Cliente cliente : lista) {
+         * System.out.println(cliente.getNmCliente());
+         * }
+         */
+        /*
+         * Servico servico = new Servico();
+         * servico.setIdServivo(1L);
+         * servico.setDsServico("Limpeza");
+         * ServicoDao servicoDao = new ServicoDao();
+         * servicoDao.insert(servico);
+         */
+        /*
+         * ServicoDao servicoDao = new ServicoDao();
+         * List<Servico> list = servicoDao.listAll();
+         * for (Servico servico : list) {
+         * System.out.println(servico.getIdServico() + servico.getDsServico());
+         * }
+         */
+
+        /*
+         * Scanner scanner = new Scanner(System.in);
+         * System.out.println("Escolha:");
+         * System.out.println("1 - Lista encadeada\n2 - Lista duplamente encadeada");
+         * ILinkedList list;
+         * int opcao = scanner.nextInt();
+         * if (opcao == 1) {
+         * list = new LinkedList();
+         * } else {
+         * list = new DoublyLinkedList();
+         * }
+         * int opcaoMenu = 9;
+         * while (opcaoMenu != 0) {
+         * System.out.println("Escolha:");
+         * System.out.println("1 - Inserir");
+         * System.out.println("2 - Remover");
+         * System.out.println("3 - Listar");
+         * System.out.println("4 - Imprimir");
+         * System.out.println("5 - Sair");
+         * opcaoMenu = scanner.nextInt();
+         * }
+         */
     }
 }
