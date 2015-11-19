a37aaccf55d978843231f123cf06c196bdd6f798
Emmanoel F <emmanoel_95@hotmail.com>
2015-11-19 17:30:52 -0200

    Aula 04

---
+++ src/modulo-05-java/Lavanderia/src/br/com/cwi/crescer/lavanderia/OutraServlet.java
@@ -0,0 +1,22 @@
+package br.com.cwi.crescer.lavanderia;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "outra", urlPatterns = { "/outra" })
+public class OutraServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String mensagem = "Qualquer mensagem. Não é a index";
+        request.setAttribute("mensagem", mensagem);
+        request.getRequestDispatcher("saida.jsp").forward(request, response);
+    }
+
+}
