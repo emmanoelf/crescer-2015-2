61c8074d43b4cd419e28ce7eecffd91a1ba0ad6b
emmanoelf <emmanoel_95@hotmail.com>
2015-11-25 10:38:38 -0200

    refactor(ClienteService) removido super do construtor, adicionado os metodos: listarClientesAtivos, buscarClientePorId, atualizar, incluir, remover

--- src/modulo-05-java/Lavanderia/src/main/java/br/com/cwi/crescer/service/ClienteService.java
+++ src/modulo-05-java/Lavanderia/src/main/java/br/com/cwi/crescer/service/ClienteService.java
@@ -1,23 +1,60 @@
 package br.com.cwi.crescer.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.cwi.crescer.dao.CidadeDAO;
 import br.com.cwi.crescer.dao.ClienteDAO;
+import br.com.cwi.crescer.domain.Cliente;
+import br.com.cwi.crescer.domain.Cliente.SituacaoCliente;
+import br.com.cwi.crescer.dto.ClienteDTO;
+import br.com.cwi.crescer.dto.ClienteResumoDTO;
+import br.com.cwi.crescer.mapper.ClienteMapper;
 
 @Service
 public class ClienteService {
 
     private ClienteDAO clienteDAO;
+    private CidadeDAO cidadeDAO;
 
     @Autowired
-    public ClienteService(ClienteDAO clienteDAO) {
-        super();
+    public ClienteService(ClienteDAO clienteDAO, CidadeDAO cidadeDAO) {
         this.clienteDAO = clienteDAO;
+        this.cidadeDAO = cidadeDAO;
     }
 
-    public String buscarPorNome(Long id) {
-        String nome = clienteDAO.findById(id).getNome();
-        return nome.toUpperCase();
+    public List<ClienteResumoDTO> listarClientesAtivos() {
+        List<Cliente> clientes = clienteDAO.findBySituacao(SituacaoCliente.ATIVO);
+        List<ClienteResumoDTO> dtos = new ArrayList<ClienteResumoDTO>();
+        for (Cliente cliente : clientes) {
+            dtos.add(new ClienteResumoDTO(cliente));
+        }
+        return dtos;
     }
+
+    public ClienteDTO buscarClientePorId(Long id) {
+        return ClienteMapper.toDTO(clienteDAO.findById(id));
+    }
+
+    public void atualizar(ClienteDTO dto) {
+        Cliente entity = clienteDAO.findById(dto.getId());
+        ClienteMapper.merge(dto, entity);
+        entity.setCidade(cidadeDAO.findById(dto.getIdCidade()));
+        clienteDAO.save(entity);
+    }
+
+    public void incluir(ClienteDTO dto) {
+        Cliente entity = ClienteMapper.getNewEntity(dto);
+        entity.setCidade(cidadeDAO.findById(dto.getIdCidade()));
+        entity.setSituacao(SituacaoCliente.ATIVO);
+        clienteDAO.save(entity);
+    }
+
+    public void remover(ClienteDTO dto) {
+        clienteDAO.remove(dto.getId());
+    }
+
 }
