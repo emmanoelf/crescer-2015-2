34d1093f3a2219c93222c3763620805149691914
emmanoelf <emmanoel_95@hotmail.com>
2015-11-25 10:33:38 -0200

    feat(ClienteDAO) adicionado metodo save e remove

--- src/modulo-05-java/Lavanderia/src/main/java/br/com/cwi/crescer/dao/ClienteDAO.java
+++ src/modulo-05-java/Lavanderia/src/main/java/br/com/cwi/crescer/dao/ClienteDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
@@ -16,12 +17,26 @@ public class ClienteDAO {
     @PersistenceContext
     private EntityManager em;
 
-    public Cliente findById(Long id){
+    public Cliente findById(Long id) {
         return em.find(Cliente.class, id);
     }
 
     public List<Cliente> findBySituacao(SituacaoCliente situacao) {
         return em.createQuery("FROM Cliente c WHERE c.situacao = :situacao", Cliente.class)
                 .setParameter("situacao", situacao).getResultList();
+    }
+
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        if (cliente.getIdCliente() == null) {
+            em.persist(cliente);
+            return cliente;
+        }
+        return em.merge(cliente);
+    }
+
+    @Transactional
+    public void remove(Long id) {
+        em.remove(em.getReference(Cliente.class, id));
     }
 }
