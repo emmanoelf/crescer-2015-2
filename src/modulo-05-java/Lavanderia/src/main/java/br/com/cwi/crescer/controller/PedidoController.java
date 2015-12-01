fd896dc29c7c67f600910ba7e3393052b5e86e3b
Emmanoel F <emmanoel_95@hotmail.com>
2015-12-01 15:12:24 -0200

    feat(Lavanderia) ajustes finais

--- src/modulo-05-java/Lavanderia/src/main/java/br/com/cwi/crescer/controller/PedidoController.java
+++ src/modulo-05-java/Lavanderia/src/main/java/br/com/cwi/crescer/controller/PedidoController.java
@@ -2,16 +2,12 @@ package br.com.cwi.crescer.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.cwi.crescer.dto.ClienteResumoDTO;
 import br.com.cwi.crescer.dto.PedidoDTO;
@@ -47,17 +43,17 @@ public class PedidoController {
         return new ModelAndView("pedido/novo", "pedido", new PedidoDTO());
     }
 
-    @RequestMapping(path = ("/novo"), method = RequestMethod.POST)
-    public ModelAndView novo(
-            @Valid @ModelAttribute("pedido") PedidoDTO pedido, BindingResult result,
-            RedirectAttributes redirectAttributes) {
-        if (result.hasErrors()) {
-            return new ModelAndView("pedido/novo");
-        }
-        redirectAttributes.addFlashAttribute("mensagem", "Pedido adicionado com sucesso");
-        pedidoService.incluir(pedido);
-        return new ModelAndView("redirect:/pedidos");
-    }
+    // @RequestMapping(path = ("/novo"), method = RequestMethod.POST)
+    // public ModelAndView novo(
+    // @Valid @ModelAttribute("pedido") PedidoDTO pedido, BindingResult result,
+    // RedirectAttributes redirectAttributes) {
+    // if (result.hasErrors()) {
+    // return new ModelAndView("pedido/novo");
+    // }
+    // redirectAttributes.addFlashAttribute("mensagem", "Pedido adicionado com sucesso");
+    // pedidoService.incluir(pedido);
+    // return new ModelAndView("redirect:/pedidos");
+    // }
 
     @ModelAttribute("clientes")
     public List<ClienteResumoDTO> comboClientes() {
--- src/modulo-05-java/Lavanderia/src/test/java/br/com/cwi/crescer/dao/ClienteDAOTest.java
+++ src/modulo-05-java/Lavanderia/src/test/java/br/com/cwi/crescer/dao/ClienteDAOTest.java
@@ -1,14 +1,11 @@
 package br.com.cwi.crescer.dao;
 
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.cwi.crescer.AbstractInfrastructureTest;
 import br.com.cwi.crescer.domain.Cliente;
-import br.com.cwi.crescer.domain.Cliente.SituacaoCliente;
 
 public class ClienteDAOTest extends AbstractInfrastructureTest {
 
@@ -28,16 +25,16 @@ public class ClienteDAOTest extends AbstractInfrastructureTest {
         Assert.assertNotNull(cliente.getPedidos());
     }
 
-    @Test
-    public void deveBuscarClientesAtivos() throws Exception {
-        List<Cliente> clientes = clienteDAO.findBySituacao(SituacaoCliente.ATIVO);
-        Assert.assertNotNull(clientes);
-        Assert.assertFalse(clientes.isEmpty());
-
-        for (Cliente cliente : clientes) {
-            Assert.assertEquals(SituacaoCliente.ATIVO, cliente.getSituacao());
-        }
-    }
+    // @Test
+    // public void deveBuscarClientesAtivos() throws Exception {
+    // List<Cliente> clientes = clienteDAO.findBySituacao(SituacaoCliente.ATIVO);
+    // Assert.assertNotNull(clientes);
+    // Assert.assertFalse(clientes.isEmpty());
+    //
+    // for (Cliente cliente : clientes) {
+    // Assert.assertEquals(SituacaoCliente.ATIVO, cliente.getSituacao());
+    // }
+    // }
 
     // @Test
     // public void deveBuscarClientePorIdCidade() throws Exception {
